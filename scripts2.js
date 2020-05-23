985fd79a58e6db5165d45e7619b2022cda903a9b
tanishghosh18 <53823388+tanishghosh18@users.noreply.github.com>
2020-05-23 08:38:20 +0530

    Add files via upload

--- scripts2.js
+++ scripts2.js
@@ -5,77 +5,101 @@ script.defer = true;
 script.async = true;
 
 // Attach your callback function to the `window` object
-function initMap(){
+function initMap() {
     var map, infoWindow;
     var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: +position.coords.latitude,
-            lng: +position.coords.longitude
-          };
+            var pos = {
+                lat: +position.coords.latitude,
+                lng: +position.coords.longitude
+            };
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pos,
-            zoom: 15,
-            mapTypeId: 'satellite'
-          });
-        
-        console.log(pos);
-          var marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            title: 'My Location'
-          });
-          marker.setMap(map);
-        
-          var markers = locations.map(function(location, i) {
-            return new google.maps.Marker({
-              position: location,
-              label: labels[i % labels.length],
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: pos,
+                zoom: 15,
+                mapTypeId: 'satellite'
             });
-          });
-    
-        
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-    
 
-          // infoWindow.setPosition(pos);
-          // infoWindow.setContent('My Location');
-          // infoWindow.open(map);
-          // map.setCenter(pos);
+            console.log(pos);
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: 'My Location'
+            });
+            marker.setMap(map);
+
+            var markers = locations.map(function(location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length],
+                });
+            });
+
+
+            var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+
+            // var markerCluster = new MarkerClusterer(map, markers, clusterStyles);
+
+
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('My Location');
+            // infoWindow.open(map);
+            // map.setCenter(pos);
 
         }, function() {
-          handleLocationError(true, map.getCenter());
-        },{timeout:10000});
-      } else {
+            handleLocationError(true, map.getCenter());
+        }, { timeout: 10000 });
+    } else {
         // Browser doesn't support Geolocation
         handleLocationError(false, map.getCenter());
-      }  
     }
+}
 
-    function handleLocationError(browserHasGeolocation, pos) {
-      alert('Error: The Geolocation service failed. Error: Your browser doesn\'t support geolocation.');
-    }
+function handleLocationError(browserHasGeolocation, pos) {
+    alert('Error: The Geolocation service failed. Error: Your browser doesn\'t support geolocation.');
+}
 document.head.appendChild(script);
 
 var locations = [
-    {lat: 26.1437697, lng: 91.5617644},
-    {lat: 26.15195, lng: 91.7671822},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.15236, lng: 91.7682792},
-    {lat: 26.1653026, lng: 91.7575289},
-    {lat: 26.170796, lng: 91.767090},
-    {lat: 26.1705957, lng: 91.7662831},
+    { lat: 26.1437697, lng: 91.5617644 },
+    { lat: 26.15195, lng: 91.7671822 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.15236, lng: 91.7682792 },
+    { lat: 26.1653026, lng: 91.7575289 },
+    { lat: 26.170796, lng: 91.767090 },
+    { lat: 26.1705957, lng: 91.7662831 },
 ]
+
+var clusterStyles = {
+    styles: [{
+            textColor: 'white',
+            height: 50,
+            url: 'm1.png',
+            width: 50
+        },
+        {
+            textColor: 'white',
+            height: 50,
+            url: 'm4.png',
+            width: 50
+        },
+        {
+            textColor: 'white',
+            height: 35,
+            url: 'images/m3.png',
+            width: 35
+        }
+    ]
+};
